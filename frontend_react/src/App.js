92599e7f6dc9764686be14582307b504a8004a1f
xavajk <xjkuehn@gmail.com>
2023-01-06 00:50:31 -0800

    Implemented containers, components, systems and started app styling

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+import { Navbar } from  './components';
+import './App.scss'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
